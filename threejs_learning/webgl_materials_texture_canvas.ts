981e589578d4d3959aca11ae7e5ddb572e35631e
zongzi531 <zongzi.xy@gmail.com>
2020-09-30 16:09:38 +0800

    feat: threejs_learning/webgl_materials_texture_canvas

--- threejs_learning/webgl_materials_texture_canvas.ts
+++ threejs_learning/webgl_materials_texture_canvas.ts
@@ -5,10 +5,10 @@ import {
   Mesh,
   BoxBufferGeometry,
   WebGLRenderer,
-  CanvasTexture,
-  Texture
+  TextureLoader
 } from 'three'
 import arina_hashimoto1215 from './assets/arina_hashimoto1215.jpg'
+import arina_hashimoto1215_2 from './assets/arina_hashimoto1215-02.jpg'
 
 let camera: PerspectiveCamera,
     scene: Scene,
@@ -17,51 +17,33 @@ let camera: PerspectiveCamera,
     renderer: WebGLRenderer
 
 const init = () => {
-  camera = new PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1 , 2000)
+  camera = new PerspectiveCamera(50, window.innerHeight / window.innerHeight, 1 , 2000)
   camera.position.z = 500
 
   scene = new Scene()
 
-  material = new MeshBasicMaterial()
+  new TextureLoader().load(arina_hashimoto1215, texture => {
+    material = new MeshBasicMaterial({ map: texture })
+    mesh = new Mesh(new BoxBufferGeometry(200, 200, 200), material)
+    scene.add(mesh)
+    renderer.render(scene, camera)
+  })
 
-  mesh = new Mesh(new BoxBufferGeometry(200, 200, 200), material)
-
-  scene.add(mesh)
+  new TextureLoader().load(arina_hashimoto1215_2, texture => {
+    scene.background = texture
+    renderer.render(scene, camera)
+  })
 
   renderer = new WebGLRenderer({ antialias: true })
   renderer.setPixelRatio(window.devicePixelRatio)
-  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setSize(window.innerHeight, window.innerHeight)
   document.body.appendChild(renderer.domElement)
 }
 
-const setupCanvas = () => {
-  const canvas = document.createElement('canvas')
-  canvas.width = 1080
-  canvas.height = 1080
-  const context = canvas.getContext('2d') as CanvasRenderingContext2D
-  material.map = new CanvasTexture(canvas)
-  const img = document.createElement('img')
-  img.src = arina_hashimoto1215
-  img.onload = () => {
-    context.drawImage(img, 0, 0, 1080, 1080)
-    ;(material.map as Texture).needsUpdate = true
-    renderer.render(scene, camera)
-  }
-}
-
-const animate = () => {
-  requestAnimationFrame(animate)
-  mesh.rotation.x += 0.01
-  mesh.rotation.y += 0.01
-  renderer.render(scene, camera)
-}
-
 init()
-setupCanvas()
-// animate()
 
 document.addEventListener('mousemove', event => {
-  const centerX = window.innerWidth / 2
+  const centerX = window.innerHeight / 2
   const centerY = window.innerHeight / 2
   const moveY = (event.x - centerX) / centerX
   const moveX = (event.y - centerY) / centerY
